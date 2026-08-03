44828b67d8907d061304d5f323e255156744e4d7
Sory-Noroc <sorinnoroc1@gmail.com>
2026-08-03 12:28:34 +0300

    step files separation

--- SeleniumBehave/features/environment.py
+++ SeleniumBehave/features/environment.py
@@ -17,20 +17,3 @@ def before_scenario(context, scenario):
 def after_scenario(context, scenario):
     if hasattr(context, "driver"):
         context.driver.quit()
-
-# @fixture
-# def browser_driver(context):
-#     # Setup
-#     context.driver = create_driver()
-#     context.page = Page(context.driver)
-#     yield context.driver
-#     # Teardown/cleanup
-#     context.driver.quit()
-#
-# fixture_registry = {
-#     "fixture.browser": browser_driver,
-# }
-#
-# def before_tag(context, tag):
-#     if tag.startswith("fixture."):
-#         return use_fixture_by_tag(tag, context, fixture_registry)
--- SeleniumBehave/features/steps/home_page_steps.py
+++ SeleniumBehave/features/steps/home_page_steps.py
@@ -1,13 +1,11 @@
-from behave import given, when, then
-from selenium.webdriver.remote.webelement import WebElement
+from behave import given, when
 
-from pages.AccountPage import AccountPage
 from pages.HomePage import HomePage
 from pages.LoginPage import LoginPage
 
 
 @given("the user is on the home page '{url}'")
-def navigate_to_login_page(context, url):
+def go_to_home_page(context, url):
     context.driver.get(url)
     assert context.page.find_element_is_visible(HomePage.ACCOUNT_LOGO_LOCATOR, 1)
 
@@ -18,26 +16,7 @@ def account_logo_click(context):
     assert context.page.find_element_is_visible(HomePage.LOGIN_BUTTON_LOCATOR, 1)
 
 @when('user enters Login page')
-def login_button_click(context):
+def login_dropdown_click(context):
     login_dropdown = context.page.find_element_and_is_clickable(HomePage.LOGIN_BUTTON_LOCATOR, 1)
     login_dropdown.click()
     assert context.page.find_element_is_visible(LoginPage.USERNAME_FIELD_LOCATOR, 1)
-
-@when("user enters login details")
-def enter_login_details(context):
-    username_field = context.page.find_element_is_visible(LoginPage.USERNAME_FIELD_LOCATOR, 1)
-    password_field = context.page.find_element_is_visible(LoginPage.PASSWORD_FIELD_LOCATOR, 1)
-    context.page.enter_text(LoginPage.USERNAME_FIELD_LOCATOR, context.username, 1)
-    context.page.enter_text(LoginPage.PASSWORD_FIELD_LOCATOR, context.password, 1)
-    assert username_field.get_attribute('value') == context.username
-
-@when("user clicks the Login button")
-def user_login_click(context):
-    login_button = context.page.find_element_is_visible(LoginPage.LOGIN_BUTTON_LOCATOR, 1)
-    login_button.click()
-    assert context.page.find_element_is_visible(AccountPage.ACCOUNT_HEADING_LOCATOR, 1)
-
-@then("user should login successfully")
-def verify_user_logged_in(context):
-    assert context.page.find_element_is_visible(AccountPage.ACCOUNT_HEADING_LOCATOR, 1)
-
