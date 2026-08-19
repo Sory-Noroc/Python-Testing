3fd24edf9f31fc08b1242f7dc19ce2f7b61c7cb5
Sory-Noroc <sorinnoroc1@gmail.com>
2026-08-19 15:01:22 +0300

    more search test scenarios

--- SeleniumBehave/features/steps/search_steps.py
+++ SeleniumBehave/features/steps/search_steps.py
@@ -4,6 +4,10 @@ from behave import *
 def search_for_product(context, product_name):
     context.current_page.search_for_product(product_name, timeout=1, with_enter=False)
 
-@then('user should see "{product_name}" product')
+@when('user enters "{product_name}" + Enter in search bar')
+def step_impl(context, product_name):
+    context.current_page.search_for_product(product_name, timeout=1, with_enter=True)
+
+@then('user should see product "{product_name}"')
 def check_product_on_page(context, product_name):
     context.current_page.find_text_on_page(product_name, timeout=1)
